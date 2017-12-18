5f52fab85ec05a9035085b41fc456a74a8a60768
popcorn <anycosmo@qq.com>
2017-12-19 00:47:16 +0800

    reids cache decorator

--- cache.py
+++ cache.py
@@ -1,34 +1,36 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-import json
+import json, base64, pickle
 from functools import wraps
 
 class RedisCache:
-    # 缓存系统类
 
     def __init__(self, redis_client):
         self._redis = redis_client
 
     def cache(self, timeout=0):
-        self._timeout = timeout
-        # Aargs: time(int): 缓存时间
-        def derecotor(func):
+        def decorator(func):
             @wraps(func)
             def wrapper(*args, **kwargs):
-                # self_args = *args
-                # self_kwargs = **kwargs
-                func_info = (self.func.__name__,args,kwargs)
-                print(func_info)
-                if self._redis.exists(func_info) and timeout != 0:
-                    result = self._redis.get(func_info)
-                    return json.dumps(result)
+                data = pickle.dumps((func.__name__,args))
+                key = base64.b64encode(data)
+
+                if timeout == 0:
+                    return func(*args, **kwargs)
+                result = self._redis.get(key)
+                if result:
+                    result = str(result,encoding='utf-8')
+                    # print(result)
+                    # result = eval(result)
+                    result = json.loads(result)
+                    # print(result,type(result))
+                    return result
                 else:
-                    new = self.func(*args,**kwargs)
-                    result = json.loads(new)
-                    new_func_info = (self.func.__name__, args, kwargs)
-                    print(new_func_info)
-                    self._redis.set(new_func_info, result, ex=self_timeout)
+                    new = func(*args,**kwargs)
+                    value = json.dumps(new)
+                    # key = func.__name__
+                    self._redis.set(key, value, ex=timeout)
                     return new
             return wrapper
-        return derecotor
+        return decorator
--- test.py
+++ test.py
@@ -1,21 +1,18 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-
 from cache import RedisCache
-import json,redis, time
+import json, redis
 
 r = redis.StrictRedis(host='127.0.0.1',db=0)
 
 cache = RedisCache(r)
 
-cache.cache(timeout=10)
+@cache.cache(timeout=10)
 def excute(args):
     data = {'name':args,'description':'test data'}
-    time.sleep(2)
-    print(json.dumps(data))
-    return json.dumps(data)
-
+    # print(json.dumps(data))
+    return data
 
 
 if __name__ == '__main__':
