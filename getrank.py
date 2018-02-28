a80c889760bbf5a4694b330a7217676b8f02232e
clikks <anycosmo@qq.com>
2018-03-01 01:36:13 +0800

    mongodb统计工具

---
+++ getrank.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+__author__ = 'lux'
+
+import sys
+
+from pymongo import MongoClient
+
+
+def get_rank(user_id):
+    client = MongoClient()
+    db = client.shiyanlou
+    contests = db.contests
+
+    group = {'$group': {'_id': '$user_id', 'score': {'$sum': '$score'}, 'submit_time': {'$sum': '$submit_time'}}}
+    sort = {'$sort': {'score': -1, 'sumit_time': 1}}
+    data = contests.aggregate([group, sort])
+    for index, item in enumerate(data):
+        if item.get('_id') == user_id:
+            rank = index + 1
+            score = item.get('score')
+            submit_time = item.get('submit_time')
+            return rank, score, submit_time
+    else:
+        print('NOTFOUND')
+        sys.exit(1)
+
+
+if __name__ == '__main__':
+    try:
+        assert len(sys.argv) == 2
+        user_id = int(sys.argv[1])
+        userdata = get_rank(user_id)
+        print(userdata)
+    except Exception as e:
+        print("Parameter Error")
